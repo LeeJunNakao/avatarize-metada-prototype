42af4e60c46c5789669b5ccf04c1e95a780d2042
Lee Jun <leejun.nakao@gmail.com>
2022-05-26 21:42:57 +0000

    feat: creaete api

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+const Collections = {
+    CuteCatz: [
+        {
+            name: "Franz Cat",
+            description: "Friendly OpenSea Creature that enjoys long swims in the ocean.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://ibb.co/Pg6MtXN",
+
+        }
+    ]
+}
+
+app.get('/:collection/:id', (req, res) => {
+    const { collection, id } = req.params;
+
+    const foundCollection = Collections[collection];
+    const metadata = foundCollection ? foundCollection[id - 1] : null;
+    res.send(metadata)
+})
+
+app.listen(PORT, () => {
+    console.log(`Runing app on port ${PORT}`)
+})
