1f8e4b4c4898f2cd3431308cf10f6bf6634946a7
github.com:LeeJunNakao <leejun.nakao@gmail.com>
2022-05-27 13:09:42 -0300

    feat: create nfts endpoint

---
+++ data.js
@@ -0,0 +1,57 @@
+const Collections = {
+    CuteCatz: [
+        {
+            name: "Franz Cat",
+            description: "Friendly OpenSea Creature that enjoys long swims in the ocean.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://i.ibb.co/9WrhGKN/Cat-04.jpg",
+        }
+    ],
+    BoredBirdz: [
+        {
+            name: "Black Feather",
+            description: "A bird pirate.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://i.ibb.co/7nmvgk8/bird-01.jpg",
+        },
+        {
+            name: "Frank",
+            description: "A bored street bird.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://i.ibb.co/Q9h72ST/bird-02.png",
+        },
+        {
+            name: "Adrian",
+            description: "A bored sailor bird.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://i.ibb.co/xCQMQMR/bird-03.png",
+        },
+        {
+            name: "Scrooge McBird",
+            description: "A bored money lover bird.",
+            external_url: "https://openseacreatures.io/3",
+            image: "https://i.ibb.co/xLjwMHy/bird-04.jpg",
+        },
+    ]
+}
+
+const Contracts = [
+    {
+        name: 'CuteCatz',
+        address: '0x4546954dB3034517Be742258bB4056c3bE8397f2'
+    },
+    {
+        name: 'CuteCats',
+        address: '0xD2D7eb762d900C053b5Afb7E403C1Ce4b6CdD892'
+    },
+    {
+        name: 'MaryNFTS',
+        address: '0x421890617953f3D7699E1B640d1aceD5bef52BF2'
+    },
+    {
+        name: 'BoredBirdz',
+        address: '0x1cF2a6Bb4Be3b288A62d1E73cD171daF7fb419A6'
+    }
+]
+
+module.exports = { Collections, Contracts }
--- main.js
+++ main.js
@@ -1,44 +1,10 @@
 const express = require('express');
+const { Collections, Contracts } = require('./data');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const Collections = {
-    CuteCatz: [
-        {
-            name: "Franz Cat",
-            description: "Friendly OpenSea Creature that enjoys long swims in the ocean.",
-            external_url: "https://openseacreatures.io/3",
-            image: "https://i.ibb.co/9WrhGKN/Cat-04.jpg",
-        }
-    ],
-    BoredBirdz: [
-        {
-            name: "Black Feather",
-            description: "A bird pirate.",
-            external_url: "https://openseacreatures.io/3",
-            image: "https://i.ibb.co/7nmvgk8/bird-01.jpg",
-        },
-        {
-            name: "Frank",
-            description: "A bored street bird.",
-            external_url: "https://openseacreatures.io/3",
-            image: "https://i.ibb.co/Q9h72ST/bird-02.png",
-        },
-        {
-            name: "Adrian",
-            description: "A bored sailor bird.",
-            external_url: "https://openseacreatures.io/3",
-            image: "https://i.ibb.co/xCQMQMR/bird-03.png",
-        },
-        {
-            name: "Scrooge McBird",
-            description: "A bored money lover bird.",
-            external_url: "https://openseacreatures.io/3",
-            image: "https://i.ibb.co/xLjwMHy/bird-04.jpg",
-        },
-    ]
-}
+
 
 app.get('/:collection/:id', (req, res) => {
     const { collection, id } = req.params;
@@ -46,6 +12,10 @@ app.get('/:collection/:id', (req, res) => {
     const foundCollection = Collections[collection];
     const metadata = foundCollection ? foundCollection[id - 1] : null;
     res.send(metadata)
+})
+
+app.get('/nfts', () => {
+    return Contracts;
 })
 
 app.listen(PORT, () => {
