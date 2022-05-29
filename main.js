923515e30d5260dba578e3f47cb54a78f8392c8c
github.com:LeeJunNakao <leejun.nakao@gmail.com>
2022-05-29 14:35:38 -0300

    feat: add collection index endpoint

--- main.js
+++ main.js
@@ -12,11 +12,20 @@ app.get('/nfts', (req, res) => {
     res.send({ contracts: Contracts });
 })
 
+app.get('/:collection', (req, res) => {
+    const { collection } = req.params;
+
+    const nfts = Collections[collection];
+
+    res.send({ nfts })
+})
+
 app.get('/:collection/:id', (req, res) => {
     const { collection, id } = req.params;
 
     const foundCollection = Collections[collection];
     const metadata = foundCollection ? foundCollection[id - 1] : null;
+
     res.send(metadata)
 })
 
